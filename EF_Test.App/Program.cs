b1fb46e58755b95359da25140b2dae0f4c093f89
Starinin Andrey <starininandrey@gmail.com>
2022-01-24 20:48:02 +0300

    Add App proj

---
+++ EF_Test.App/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using EF_Test.DB_Lib;
+
+namespace EF_Test.App
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var db = new PersonsDb();
+            var persons = db.TablePersons;
+
+            /*db.TablePersons.Add(new Person
+            {
+                FirstName = "Anonim",
+                LastName = "Anonimus",
+                Age = 0,
+                IsDelete = true
+            });
+            db.SaveChanges();*/
+            
+            foreach (var p in persons.ToList())
+            {
+                Console.WriteLine($"{p.Id}: {p.LastName} {p.FirstName}, {p.Age}");
+            }
+        }
+    }
+}
