b03812f82f6606e08c59f6391407b65bb45a8018
Starinin Andrey <starininandrey@gmail.com>
2022-01-24 20:25:40 +0300

    Add DB_Lib proj

---
+++ EF_Test.DB_Lib/PersonsDb.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using EF_Test.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF_Test.DB_Lib
+{
+    public class PersonsDb : DbContext
+    {
+        public DbSet<Person> TablePersons { get; set; }
+        
+        public PersonsDb()
+        {
+            Database.EnsureCreated();
+        }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL(GetConnectionString("connection_to_db.txt"));
+        }
+
+        private static string GetConnectionString(string path)
+        {
+            return File.ReadAllText(path);
+        }
+    }
+}
